6311f597b896040526b1e042089ff4d5f93ba26e
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-01 14:55:05 -0300

    feat: att message of error in middleware findUserById

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function findUserById(request, response, next) {
 
   const user = users.find((user) => user.id === id);
 
-  if (!user) return response.status(404);
+  if (!user) return response.status(404).json({ error: "User not found." });
 
   request.user = user;
   return next();
